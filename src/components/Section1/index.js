c71bfe49768bb10acb76ebcf8f604f2e51ec6a9c
Afeer <afeerr@gmail.com>
2021-04-06 09:21:41 +0530

    ui update

--- src/components/Section1/index.js
+++ src/components/Section1/index.js
@@ -4,8 +4,8 @@ import "./style.css";
 const Section1 = () => {
   return (
     <div className="section1">
-      <div>Start your dream home journey with just Rs. 25,000/-</div>
-      <div>Payment Portal</div>
+      <div className="sect1-intro"><span className="sect1-introtext">Start your dream home journey with just Rs. 25,000/-</span></div>
+      <div className="sect1-payment"><span>Payment Portal</span></div>
     </div>
   );
 };
--- src/components/Section2/index.js
+++ src/components/Section2/index.js
@@ -4,13 +4,36 @@ import "./style.css";
 const Section2 = () => {
   return (
     <div className="section2">
-      <ul>
-        <li>Construction</li>
-        <li>Interior</li>
-        <li>Projects</li>
-        <li>Testimonials</li>
-        <li>Blog</li>
-      </ul>
+      <div className="item">
+        <header class="header">
+          {/* <p>Sep 11th 2020</p> */}
+          <h3>Construction</h3>
+        </header>
+      </div>
+      <div className="item">
+        <header class="header">
+          {/* <p>Sep 11th 2020</p> */}
+          <h3>Interior</h3>
+        </header>
+      </div>
+      <div className="item">
+        <header class="header">
+          {/* <p>Sep 11th 2020</p> */}
+          <h3>Projects</h3>
+        </header>
+      </div>
+      <div className="item">
+        <header class="header">
+          {/* <p>Sep 11th 2020</p> */}
+          <h3>Testimonials</h3>
+        </header>
+      </div>
+      <div className="item">
+        <header class="header">
+          {/* <p>Sep 11th 2020</p> */}
+          <h3>Blog</h3>
+        </header>
+      </div>
     </div>
   );
 };
