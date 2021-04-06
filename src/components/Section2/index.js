cb68cee895c7a2a80351125a20d33179abc06b27
Afeer <afeerr@gmail.com>
2021-04-06 19:50:48 +0530

    fixed image issue

--- src/components/Section2/index.js
+++ src/components/Section2/index.js
@@ -12,36 +12,36 @@ const Section2 = () => {
       <div className="item">
         <header class="header">
           {/* <p>Sep 11th 2020</p> */}
+          <img src={Construction} alt="construction" height="180px" />
           <h3>Construction</h3>
-          <img src={Construction} height="180px" />
         </header>
       </div>
       <div className="item">
         <header class="header">
           {/* <p>Sep 11th 2020</p> */}
+          <img src={Interior} alt="interior" height="180px" />
           <h3>Interior</h3>
-          <img src={Interior} height="180px" />
         </header>
       </div>
       <div className="item">
         <header class="header">
           {/* <p>Sep 11th 2020</p> */}
+          <img src={Projects} alt="projects" height="180px" />
           <h3>Projects</h3>
-          <img src={Projects} height="180px" />
         </header>
       </div>
       <div className="item">
         <header class="header">
           {/* <p>Sep 11th 2020</p> */}
+          <img src={Testimonials} alt="testimonials" height="180px" />
           <h3>Testimonials</h3>
-          <img src={Testimonials} height="180px" />
         </header>
       </div>
       <div className="item">
         <header class="header">
           {/* <p>Sep 11th 2020</p> */}
+          <img src={Blog} alt="blog" height="180px" />
           <h3>Blog</h3>
-          <img src={Blog} height="180px" />
         </header>
       </div>
     </div>
