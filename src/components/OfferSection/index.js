128ada6f41ac2e7be36779a8ce5bf9385f09fcb8
Afeer <afeerr@gmail.com>
2021-04-06 11:14:47 +0530

    update css

--- src/components/OfferSection/index.js
+++ src/components/OfferSection/index.js
@@ -4,13 +4,13 @@ import "./style.css";
 const OfferSection = () => {
   return (
     <div className="offer-section">
-      <h1 className="offer-header">Offers and Contests</h1>
+      <h2 className="offer-header">Offers and Contests</h2>
       <div className="offer-items">
         <div className="offer-card">
-          <p>Rs. 1600/sqft for Home Construction</p>
+          <h3>Rs. 1600/sqft for Home Construction</h3>
         </div>
         <div className="offer-card">
-          <p>Rs. 1.99L for Home Interior</p>
+          <h3>Rs. 1.99L for Home Interior</h3>
         </div>
       </div>
     </div>
