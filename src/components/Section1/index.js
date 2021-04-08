fd0b9c67a6aa7086621c0ecd45074c97fab127b2
Afeer <afeerr@gmail.com>
2021-04-08 11:49:45 +0530

    fixed banner and size of cards

--- src/components/Section1/index.js
+++ src/components/Section1/index.js
@@ -6,7 +6,13 @@ const Section1 = () => {
   return (
     <div className="section1">
       <div className="sect1-intro">
-        <img src={Banner} alt="banner" height="100px" width="1000px" />
+        <img
+          src={Banner}
+          alt="banner"
+          height="80px"
+          width="800px"
+          style={{ borderRadius: "24px" }}
+        />
         {/* <span className="sect1-introtext">Start your dream home journey with just Rs. 25,000/-</span> */}
       </div>
       <div className="sect1-payment">
