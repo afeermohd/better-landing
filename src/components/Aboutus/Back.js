93d916d4245a8b14626a0e7fff94ebdbc8cc03d6
Afeer <afeerr@gmail.com>
2021-04-13 08:38:30 +0530

    changed text card flip

--- src/components/Aboutus/Back.js
+++ src/components/Aboutus/Back.js
@@ -6,15 +6,8 @@ const Back = () => {
     <div className="back aboutus">
       <div className="about-card">
         <h2 className="about-title">Why Us</h2>
-        <p className="about-para">
-          Some sample text to demonstrate how these cards will work, including
-          how they truncate long sentences. This section displays the
-          full-length blog post.
-        </p>
-        <p className="about-para">
-          Bloggity bloggity bloggity blog. This would be the full text of the
-          abbreviated blog post.
-        </p>
+        <p className="about-para">Some text</p>
+        <p className="about-para">Some Text</p>
       </div>
     </div>
   );
