e91e2e9da03823019b951693825e4756eca90fdc
afeermohd <afeerr@gmail.com>
2021-04-14 18:42:11 +0530

    add categories

--- src/components/Categories/index.js
+++ src/components/Categories/index.js
@@ -87,24 +87,39 @@ const Categories = () => {
         </div>
       ) : (
         <div onClick={closeInfo} className="modal-container">
+          {/* <Construction open={showInfo === "construction"} /> */}
           <Modal open={showInfo === "construction"}>
-            <h2>Construction</h2>
-            <p>
-              This is Construction Content The Better Co is a home construction
-              company which helps you build your dream home and do your home
-              interiors on one platform. We have an experienced team consisting
-              of Designers, Architects, Engineers, Experience management,
-              Quality assurance, etc., to provide you with the best home
-              construction possible within your budget. We supply you with the
-              best service and leave no chance for disappointment. Quality and
-              experience is our priority over everything else. Serving all
-              client needs of building their dream home within their budget is
-              our speciality. We envision providing people the power of design,
-              track work progress, quality and regulate their budget to bring
-              forth the home of their dreams into a tangible material via
-              Product, Material, Financial and Technological innovations.
-              Customer Satisfaction is our go-to motive.
-            </p>
+            <div className="modal-list">
+              <h3>Packages</h3>
+              <div>Material List</div>
+              <div>Projects</div>
+              <div>Offers</div>
+              <div>Gallery</div>
+              <div>Blog</div>
+              <div>review</div>
+            </div>
+            <div className="modal-inside">
+              <h2>Construction</h2>
+              <div>
+                <div>
+                  <span>Standard Package</span>
+                  <span>|</span>
+                  <span>Premium Package</span>
+                </div>
+                <ul>
+                  <li>Architectural Design and Drawing</li>
+                  <li>Structure</li>
+                  <li>Kitchen</li>
+                  <li>Bathroom</li>
+                  <li>Doors and Windows</li>
+                  <li>Painting & Accent Walls</li>
+                  <li>Electrical</li>
+                  <li>Flooring</li>
+                  <li>Other Inclusions</li>
+                  <li>Additional Charge</li>
+                </ul>
+              </div>
+            </div>
           </Modal>
           <Modal open={showInfo === "interior"}>
             <h2>Interior</h2>
