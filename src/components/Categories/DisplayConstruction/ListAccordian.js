c73e365ee6dd5325a5122fe539aea8967373be9f
Afeer <afeerr@gmail.com>
2021-04-16 12:31:02 +0530

    added list accordian

--- src/components/Categories/DisplayConstruction/ListAccordian.js
+++ src/components/Categories/DisplayConstruction/ListAccordian.js
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./ListAccordian.css";
+import Chevron from "./Chevron";
 
 const ListAccordian = ({ title, content }) => {
+  const [active, setActive] = useState("");
+  const [height, setHeight] = useState("0px");
+  const [rotate, setRotate] = useState("listaccordian-icon");
+
+  const contentRef = useRef(null);
+
+  const toggleAccordian = () => {
+    setActive(active === "" ? "active" : "");
+    setHeight(
+      active === "active" ? "0px" : `${contentRef.current.scrollHeight}px`
+    );
+    setRotate(
+      active === "active" ? "listaccordian-icon" : "listaccordian-icon rotate"
+    );
+  };
+
   return (
     <div className="listaccodian-section">
-      <button className="listaccordian">
+      <button className={`listaccordian ${active}`} onClick={toggleAccordian}>
+        <Chevron className={`${rotate}`} width={10} fill={"#ccc"} />
         <p className="listaccordian-title">{title}</p>
       </button>
       <div
-        className="listaccordian-text"
-        dangerouslySetInnerHTML={{ __html: content }}
-      ></div>
+        ref={contentRef}
+        style={{ maxHeight: `${height}` }}
+        className="listaccordian-content"
+      >
+        <div
+          className="listaccordian-text"
+          dangerouslySetInnerHTML={{ __html: content }}
+        />
+      </div>
     </div>
   );
 };
