e07659622557ae9b0b39abdc0278843d168973bb
Afeer <afeerr@gmail.com>
2021-04-20 15:47:22 +0530

    gallery

--- src/components/Categories/DisplayConstruction/DisplayListInfo.js
+++ src/components/Categories/DisplayConstruction/DisplayListInfo.js
@@ -2,6 +2,12 @@ import React, { useState } from "react";
 import DisplayMaterials from "./DisplayContent/DisplayMaterials";
 import DisplayProjects from "./DisplayContent/DisplayProjects";
 import DisplaySelectedPackage from "./DisplayContent/DisplaySelectedPackage";
+import Marquee from "react-fast-marquee";
+import Construction from "../../../assets/construction.jpeg";
+import Interior from "../../../assets/interior.jpeg";
+import Projects from "../../../assets/projects.jpeg";
+import Blog from "../../../assets/blog.jpeg";
+import Testimonials from "../../../assets/testimonials.jpeg";
 
 const DisplayListInfo = ({ list }) => {
   const [selectPackage, setSelectPackage] = useState("standard");
@@ -60,7 +66,69 @@ const DisplayListInfo = ({ list }) => {
   } else if (list === "gallery") {
     return (
       <div>
-        {/* <h1>Gallery</h1> */} <div>this </div>
+        {/* <h1>Gallery</h1> */}{" "}
+        {/* <div>
+          <marquee behaviour="alternate" className="marquee-gallery">
+            <img src="https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8&w=1000&q=80" />
+            <img src="https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8&w=1000&q=80" />
+            <img src="https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8&w=1000&q=80" />
+            <img src="https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8&w=1000&q=80" />
+            <img src="https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8&w=1000&q=80" />
+          </marquee>
+          <marquee
+            behaviour="scroll"
+            direction="right"
+            className="marquee-gallery"
+            onMouseOver="stop()"
+            onMouseOut="start()"
+          >
+            <img src="https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8&w=1000&q=80" />
+            <img src="https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8&w=1000&q=80" />
+            <img src="https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8&w=1000&q=80" />
+            <img src="https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8&w=1000&q=80" />
+           
+          </marquee>
+        </div> */}
+        <Marquee
+          className="gallery-marq"
+          // pauseOnHover="true"
+          speed="60"
+          gradientColor="[0, 0, 0]"
+          gradirent="true"
+        >
+          <img src={Construction} alt="construction" />
+          {/* <img src={Interior} alt="construction"/> */}
+          <img src={Testimonials} alt="construction" />
+          <img src={Blog} alt="construction" />
+          <img src={Projects} alt="construction" />
+        </Marquee>
+        <Marquee
+          className="gallery-marq"
+          // pauseOnHover="true"
+          speed="60"
+          gradientColor="[0, 0, 0]"
+          gradirent="true"
+          direction="right"
+        >
+          {/* <img src={Construction} /> */}
+          <img src={Interior} alt="construction" />
+          <img src={Testimonials} alt="construction" />
+          <img src={Blog} alt="construction" />
+          <img src={Projects} alt="construction" />
+        </Marquee>
+        <Marquee
+          className="gallery-marq"
+          // pauseOnHover="true"
+          speed="60"
+          gradientColor="[0, 0, 0]"
+          gradirent="true"
+        >
+          <img src={Construction} alt="construction" />
+          <img src={Interior} alt="construction" />
+          {/* <img src={Testimonials} alt="construction" /> */}
+          <img src={Blog} alt="construction" />
+          <img src={Projects} alt="construction" />
+        </Marquee>
       </div>
     );
   } else if (list === "blog") {
